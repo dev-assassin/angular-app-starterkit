1da6f016b2c4a5adf3400ba0ea364d92909a3bed
dev-assassin <alexandra.uefa@gmail.com>
2018-09-03 14:27:29 +0800

    update header component

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { FormioAuthService } from 'angular-formio/auth';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.sass']
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(public auth: FormioAuthService) {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
 }
