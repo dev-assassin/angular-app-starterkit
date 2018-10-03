70dbeb4656f362f6518147bc8bbc475df8786d9d
dev-assassin <alexandra.uefa@gmail.com>
2018-10-03 10:40:46 +0800

    add home component ts and fix bug

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormioAuthService } from 'angular-formio/auth';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(public auth: FormioAuthService) { }
 
   ngOnInit() {
+
   }
 
 }
